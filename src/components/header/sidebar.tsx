155f7c99929328584a6e965b9e4a1d9b2175b338
matiibuffaRC <matbuffautn05@gmail.com>
2026-05-08 20:14:01 -0300

    Solucion del menu mobile

--- src/components/header/sidebar.tsx
+++ src/components/header/sidebar.tsx
@@ -14,9 +14,9 @@ function Sidebar({ open, setOpen, onClick }: SidebarProps) {
         <div className={`fixed top-0 left-0 h-full w-64 bg-gray-100 text-[#151b23] dark:bg-[#151B23] dark:text-[#D1D7E0] z-50 transform transition-transform duration-300 p-5 ${ open ? "translate-x-0" : "-translate-x-full" }`}>
             <nav className="flex flex-col gap-4 text-lg font-bold urbanist">
                 <a href="#inicio" onClick={onClick}>Inicio</a>
-                <a href="#sobre-mi" onClick={onClick}>Sobre mí</a>
                 <a href="#proyectos" onClick={onClick}>Proyectos</a>
-                <a href="#contacto" onClick={onClick}>Contacto</a>
+                <a href="#experiencia" onClick={onClick}>Experiencia</a>
+                <a href="#sobre-mi" onClick={onClick}>Sobre mí</a>
             </nav>
         </div>
         
