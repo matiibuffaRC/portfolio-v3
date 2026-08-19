601d59c2930f175b0654f0a706705efce6792845
matiibuffaRC <matbuffautn05@gmail.com>
2026-08-19 15:58:27 -0300

    Corregimos import

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Proyects from "./components/Home/proyects/Proyects.tsx";
 import Experience from "./components/Home/Experience.tsx";
 import AboutMe from "./components/Home/AboutMe.tsx";
 import TechCarousel from "./components/Home/Techcarousel.tsx";
-import Footer from "./components/footer/Footer.tsx";
+import Footer from "./components/footer/Footer.tsx"
 //
 
 // Import dependencias
