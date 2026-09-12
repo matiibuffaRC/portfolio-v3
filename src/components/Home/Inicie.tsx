87eaadc95b74609ab42b73d83eabe1e28e05e38a
matiibuffaRC <matbuffautn05@gmail.com>
2026-09-12 19:41:59 -0300

    Retocamos inicie component

--- src/components/Home/Inicie.tsx
+++ src/components/Home/Inicie.tsx
@@ -15,13 +15,13 @@ function Inicie() {
                 </h2>
                 
                 <div className="animate-fade-in-up [animation-delay:450ms] flex flex-row justify-center gap-2 px-4 my-3 w-full">
-                    <div className="bg-[#3998B6] max-w-32 flex-1 text-center py-1 rounded-2xl transition-colors hover:bg-[#c9ecea]">
-                        <a title="Descargar curriculum vitae" aria-label="Descargar curriculum vitae de Matías Buffa" href="/MatiasBuffaCVP.pdf" target="_blank" rel="noopener noreferrer" className="block text-white urbanist font-bold" >
+                    <div className="bg-[#3998B6] max-w-32 flex-1 text-center py-1 rounded-4xl transition-colors hover:bg-[#c9ecea]">
+                        <a title="Descargar curriculum vitae" aria-label="Descargar curriculum vitae de Matías Buffa" href="/MatiasBuffaCVP.pdf" target="_blank" rel="noopener noreferrer" className="text-lg block text-white urbanist font-bold select-none" >
                         Ver CV
                         </a>
                     </div>
-                    <div className="bg-transparent max-w-32 flex-1 text-center py-1 rounded-2xl transition-colors hover:bg-[#c9ecea] border border-[#3998B6] dark:border-white">
-                        <a title="Visitar LinkedIn de Matías Buffa" aria-label="Visitar perfil de LinkedIn de Matías Buffa" href="https://www.linkedin.com/in/mat%C3%ADas-buffa-b4b901358/" target="_blank" rel="noopener noreferrer" className="block text-[#259E93] dark:text-white urbanist font-bold" >
+                    <div className="bg-transparent max-w-32 flex-1 text-center py-1 rounded-4xl transition-colors hover:bg-[#c9ecea] border border-[#3998B6] dark:border-white">
+                        <a title="Visitar LinkedIn de Matías Buffa" aria-label="Visitar perfil de LinkedIn de Matías Buffa" href="https://www.linkedin.com/in/mat%C3%ADas-buffa-b4b901358/" target="_blank" rel="noopener noreferrer" className="text-lg block text-[#259E93] dark:text-white urbanist font-bold select-none" >
                         LinkedIn
                         </a>
                     </div>
